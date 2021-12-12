6c587368aeafde8b08f6a8f35d46c65389b3a543
mehdi <kamani@ipreez.ir>
2021-12-12 17:09:31 +0330

    update demo

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,5 +1,18 @@
 <template>
+<div>
 	<div id="map"></div>
+	<WFS-search
+		:debounce-delay="350"
+		wfs-url="http://localhost:8600/geoserver/wfs"
+		query-template="*%v*"
+		:type-names="['iran:state','iran:county','iran:city','iran:roads', 'iran:forestpark','iran:lakes', 'iran:district', 'iran:settlements']"
+		v-model="srchResult"
+	>
+		<template slot-scope="{ term, update }">
+			<input type="text" :v-model="term" v-on:input="update($event.target.value)">
+		</template>
+	</WFS-search>
+</div>
 </template>
 
 <script>
@@ -13,39 +26,53 @@
 	import VectorSource from 'ol/source/Vector';
 	import {Stroke, Style} from 'ol/style';
 	import {Vector as VectorLayer} from 'ol/layer';
-	import {bbox as bboxStrategy} from 'ol/loadingstrategy';
+
+	import WFSSearch from '@/components/WFSSearch'
 
 
 export default {
 	name: "Map",
-	props: {
-
+	components: {
+		WFSSearch,
+		Toggle,
 	},
 	data() {
 		return {
-			map: null
+			found: [],
+			map: null,
+			vectorSource: null,
+			srchResult: null,
 		}
 	},
-	created() {
-			
-	},
 	mounted() {
-		const vectorSource = new VectorSource({
-			format: new GeoJSON(),
-			url: function (extent) {
-				return (
-					'http://localhost:8600/geoserver/wfs?service=WFS&' +
-					'version=1.1.0&request=GetFeature&typename=iran:water&' +
-					'outputFormat=application/json&srsname=EPSG:3857&' +
-					'bbox=' +
-					extent.join(',') +
-					',EPSG:3857'
-				);
-			},
-			strategy: bboxStrategy,
-		});
+		// const vectorSource = new VectorSource({
+		// 	format: new GeoJSON(),
+		// 	url: function (extent) {
+		// 		return (
+		// 			'http://localhost:8600/geoserver/wfs?service=WFS&' +
+		// 			'version=1.1.0&request=GetFeature&typename=iran:water&' +
+		// 			'outputFormat=application/json&srsname=EPSG:3857&' +
+		// 			'bbox=' +
+		// 			extent.join(',') +
+		// 			',EPSG:3857'
+		// 		);
+		// 	},
+		// 	strategy: bboxStrategy,
+		// });
+		// const vector = new VectorLayer({
+		// 	source: vectorSource,
+		// 	style: new Style({
+		// 		stroke: new Stroke({
+		// 			color: 'rgba(0, 0, 255, 1.0)',
+		// 			width: 2,
+		// 		}),
+		// 	}),
+		// });
+
+
+		this.vectorSource = new VectorSource();
 		const vector = new VectorLayer({
-			source: vectorSource,
+			source: this.vectorSource,
 			style: new Style({
 				stroke: new Stroke({
 					color: 'rgba(0, 0, 255, 1.0)',
@@ -54,11 +81,10 @@ export default {
 			}),
 		});
 
-
 		const layers = [
 			new TileLayer({
 				source: new TileWMS({
-					url: 'http://localhost:8600/geoserver/wms',
+					url: 'http://192.168.34.42:8600/geoserver/wms',
 					params: {'LAYERS': 'iran:gp-iran', 'TILED': true, 'VERSION': '1.3.0'},
 					serverType: 'geoserver',
 					transition: 0,
@@ -71,15 +97,33 @@ export default {
 			target: 'map',
 			view: new View({
 				center: [5964457.55554387, 3884488.746230995],
-				zoom: 7,
+				zoom: 5,
 			}),
 		});
 		this.map.on('click', (evt) => {
 			console.log(evt, evt.coordinate)
 		})
-	}
+	},
+	methods: {
+	},
+	watch: {
+		srchResult(val) {
+			this.vectorSource.clear();
+			if (!val.numberReturned) return
+			const features = new GeoJSON().readFeatures(val);
+			this.vectorSource.addFeatures(features);
+			this.map.getView().fit(this.vectorSource.getExtent(), {
+				duration: 450,
+
+			});
+		}
+	},
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+#map {
+	height: 100%;
+	width: 100%
+}
 </style>
